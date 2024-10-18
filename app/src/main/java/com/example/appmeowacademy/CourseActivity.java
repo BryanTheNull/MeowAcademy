c48f10a7d9398d3650b4243a21666675fb11d28f
Bryan Vera <bv58750@gmail.com>
2024-10-18 17:07:42 -0300

    3 CARGA

--- app/src/main/java/com/example/appmeowacademy/CourseActivity.java
+++ app/src/main/java/com/example/appmeowacademy/CourseActivity.java
@@ -1,6 +1,7 @@
 package com.example.appmeowacademy;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -58,6 +59,13 @@ public class CourseActivity extends AppCompatActivity {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void onClickGoogle(View view) {
+        // Intent para abrir el navegador con la URL del curso
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.coursera.org/courseraplus/?utm_medium=sem&utm_source=gg&utm_campaign=B2C_LATAM_coursera_FTCOF_courseraplus&campaignid=20844412474&adgroupid=159999536361&device=c&keyword=coursera&matchtype=b&network=g&devicemodel=&adposition=&creativeid=683667892738&hide_mobile_promo&gad_source=1&gclid=CjwKCAjwjsi4BhB5EiwAFAL0YJHzFN03OR6k1T62YoInjTaeWqIrT04ppKcSVfsFn7wtKRLJBTcEhRoCYqwQAvD_BwE"));
+        startActivity(intent);
     }
 
 }
